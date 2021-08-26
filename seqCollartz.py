c40afd0b8c739e9e65738ad2ab3aad46e93abd16
carlos-joker <mgf.carloshenrique20@gmail.com>
2021-08-26 03:23:17 -0300

    Programas em Python

--- seqCollartz.py
+++ seqCollartz.py
@@ -17,11 +17,27 @@ def collatz(number):
             a = 3 * a + 1
             lista.append(a)
     print(lista)
+    print('\nTamanho da lista: {} numeros'.format(len(lista)))
+    a = input('Deseja fazer um novo calculo? (s/n)')
+    if a.lower() == 's':
+        print('\n')
+        return menu()
+    else:
+        print('Encerrando...')
     
 def menu():
     try:
-        number = int(input('Digite um numero: '))
-        return collatz(number)
+        number = int(input('Digite um numero positivo  e diferente de zero: '))
+        if number < 0:
+            print('Você digitou um numero negativo!!!')
+            print('tente novamente.')
+            return menu()
+        elif number == 0:
+            print('Você digitou um valor zero!!!')
+            print('tente novamente.')
+            return menu()
+        else:
+            return collatz(number)
     except:
         print('Você não digitou um número!!!')
         return menu()
